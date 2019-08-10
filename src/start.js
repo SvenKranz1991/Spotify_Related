bd3ca1db4cf5a41d8ec78a8a30403d5395e3735a
Sven Kranz <svenkranz@t-online.de>
2019-08-10 13:40:16 +0200

    Setup Start React Babel for Playlist Project

---
+++ src/start.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<HelloWorld />, document.querySelector("main"));
+
+function HelloWorld() {
+    return <div>Hello, Spotify Project!</div>;
+}
