229e02ab0d64f223037683936e9b6ccacf446619
Sven Kranz <svenkranz@t-online.de>
2019-08-14 22:51:28 +0200

    Audio Feature for Tracklist works, working on single Fetch

--- src/audioFeature.js
+++ src/audioFeature.js
@@ -1,37 +1,80 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "./axios";
 
-export default function AudioFeature(props) {
-    const [tracksFeatures, setTopArtists] = useState();
-
-    console.log("My List Info: ", tracksFeatures);
-
-    console.log(props);
-
-    let trackId = props.trackId;
-
-    useEffect(() => {
+export default class AudioFeature extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    componentDidMount() {
+        let trackId = this.props.trackId;
         axios
             .get(`/audioFeature/${trackId}.json`)
             .then(result => {
                 console.log("My AudioFeature Result: ", result);
-                setTopArtists(result.data.data);
+
+                let {
+                    id,
+                    acousticness,
+                    danceability,
+                    energy,
+                    instrumentalness,
+                    key,
+                    liveness,
+                    loudness,
+                    mode,
+                    speechiness,
+                    tempo,
+                    time_signature,
+                    uri,
+                    valence
+                } = result.data.data;
+
+                this.setState({
+                    id: id,
+                    acousticness: acousticness,
+                    danceability: danceability,
+                    energy: energy,
+                    instrumentalness: instrumentalness,
+                    key: key,
+                    liveness: liveness,
+                    loudness: loudness,
+                    mode: mode,
+                    speechiness: speechiness,
+                    tempo: tempo,
+                    time_signature: time_signature,
+                    uri: uri,
+                    valence: valence
+                });
             })
             .catch(err => {
                 console.log("Error in getting Features: ", err);
             });
-    }, []);
-    return (
-        <div className="playlistsWrapper">
-            <h3>Audio Feature</h3>
-            {tracksFeatures &&
-                tracksFeatures.map(list => (
-                    <div key={list.id}>
-                        <p>{list.acousticness}</p>
-                        <p>Popularity - {list.danceability}</p>
-                        <p>Followers - {list.energy}</p>
-                    </div>
-                ))}
-        </div>
-    );
+    }
+
+    render() {
+        return (
+            <div className="playlistsWrapper">
+                <h3>Audio Feature</h3>
+                <a
+                    href={this.state.uri}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Link to Track
+                </a>
+                <div>
+                    <p>Acousticness -{this.state.acousticness}</p>
+                    <p>Popularity - {this.state.danceability}</p>
+                    <p>Energy - {this.state.energy}</p>
+                    <p>Instrumentalness - {this.state.instrumentalness}</p>
+                    <p>Liveness - {this.state.liveness}</p>
+                    <p>Loudness - {this.state.loudness}</p>
+                    <p>Speechiness - {this.state.speechiness}</p>
+                    <p>Valence - {this.state.valence}</p>
+                    <p>Beats per Minute - {this.state.tempo}</p>
+                </div>
+            </div>
+        );
+    }
 }
